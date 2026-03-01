469cec590f349f97ee5a187e5a396f3c785073a7
DSA-rec <venugopala1526@gmail.com>
2026-03-01 11:05:53 +0530

    selection sort

---
+++ select.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class select {
+     public static void select(int arr[]){
+
+         int n= arr.length;
+         for(int i=0;i<n-1;i++){
+             int minindex=i;
+             for(int j=i+1;j<=n-1;j++){
+                 if(arr[j]<arr[minindex]){
+                     minindex=j;
+                 }
+             }
+             int temp = arr[i];
+             arr[i]=arr[minindex];
+             arr[minindex]=temp;
+         }
+
+
+     }
+
+
+    static void main() {
+         int[] arr= {2,7,5,9,8};
+         select(arr);
+         for(int i=0;i<arr.length;i++){
+             System.out.println(" "+arr[i]);
+         }
+
+
+    }
+}
